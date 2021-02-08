3efa955388b55329bd3ef15332e3ec88e1584628
GabrielBorba2019 <gabrielborba.pm@Gmail.com>
2021-02-08 12:22:55 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -17,7 +17,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 @Entity
-@Table(name = "tb_users")
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
